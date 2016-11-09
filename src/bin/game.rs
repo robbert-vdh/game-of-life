cb4bd3aca822a90a7ececad1f4d677fcbbe603df
Robbert van der Helm <robbert@zzindi.nl>
2016-11-09 13:02:15 +0100

    Verplaats de shaders naar eigen bestanden

--- src/bin/game.rs
+++ src/bin/game.rs
@@ -22,25 +22,8 @@ fn main() {
     let indices = index::NoIndices(index::PrimitiveType::TrianglesList);
 
     let program = {
-        let vertex_shader_src = r#"
-                #version 140
-
-                in vec2 position;
-
-                void main() {
-                    gl_Position = vec4(position, 0.0, 1.0);
-                }
-            "#;
-
-        let fragment_shader_src = r#"
-                #version 140
-
-                out vec4 color;
-
-                void main() {
-                    color = vec4(1.0, 0.0, 0.0, 1.0);
-                }
-            "#;
+        let vertex_shader_src = include_str!("../assets/vs.glsl");
+        let fragment_shader_src = include_str!("../assets/fs.glsl");
 
         Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap()
     };
