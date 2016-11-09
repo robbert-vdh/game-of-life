558e7e5f2e41c220e3c1eb293ca6cf43cf55b5ac
Robbert van der Helm <robbert@zzindi.nl>
2016-11-09 17:12:41 +0100

    Make the colors in the vector shader more interesting

--- src/assets/fs.glsl
+++ src/assets/fs.glsl
@@ -1,7 +1,9 @@
 #version 140
 
+in vec2 f_position;
+
 out vec4 color;
 
 void main() {
-    color = vec4(1.0, 0.5, 0.0, 1.0);
+    color = vec4(f_position.xy, 1.0, 1.0);
 }
--- src/assets/vs.glsl
+++ src/assets/vs.glsl
@@ -2,6 +2,10 @@
 
 in vec2 v_position;
 
+out vec2 f_position;
+
 void main() {
+    f_position = v_position;
+
     gl_Position = vec4(v_position, 0.0, 1.0);
 }
